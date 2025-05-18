80a2daaaaefa8442c89938616a8f79c540be1788
dev-netinho <jose.gc.neto@aluno.uepa.br>
2025-05-18 14:10:36 -0300

    Commit de mais um deploy v6

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ class Database {
         this.connection = sequelize;
         models.forEach(model => model.init(this.connection));
         this.associate();
-        await this.connection.sync(); 
+        // Não chamamos sync aqui para evitar duplicação
     }
 
     associate() {
@@ -22,6 +22,11 @@ class Database {
                 model.associate(this.connection.models);
             }
         });
+    }
+    
+    // Método sync exposto para ser chamado externamente
+    sync(options) {
+        return this.connection.sync(options);
     }
 }
 
